e452228714b5b90479ef5e62ef99f4ed85e150f7
Nikolay Pohlupkov <nikolaypoh@gmail.com>
2017-10-08 22:29:20 +0300

    Add winner highlights from Medium tutorial

--- src/components/OChar.jsx
+++ src/components/OChar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const OChar = () => {
+const OChar = (props) => {
   return (
-    <div className="char char--o">
+    <div className={`char char--o column${props.position}`}>
       <svg viewBox="0 0 56 56">
         <circle cx={28} cy={28} r={25} stroke="black" strokeWidth="1" fill="none" />
       </svg>
     </div>
   );
+};
+
+OChar.propTypes = {
+  position: PropTypes.number.isRequired,
 };
 
 export default OChar;
--- src/components/XChar.jsx
+++ src/components/XChar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const XChar = () => {
+const XChar = (props) => {
   return (
-    <div className="char char--x">
+    <div className={`char char--x column${props.position}`}>
       <svg viewBox="0 0 56 56">
         <line x1="2" y1="2" x2="54" y2="54" stroke="black" strokeWidth="1" />
         <line x1="2" y1="54" x2="54" y2="2" stroke="black" strokeWidth="2" />
       </svg>
     </div>
   );
+};
+
+XChar.propTypes = {
+  position: PropTypes.number.isRequired,
 };
 
 export default XChar;
